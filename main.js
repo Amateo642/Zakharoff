1480d8f74f91eedae58dd3b44915f176866f3c43
Amateo642 <KitlyaevDD@mail.ru>
2020-09-21 13:25:54 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,81 @@
+/*
+//string
+let firstName = 'Zar';
+firstName = 'Amateo';
+const lastName = 'Goodman';
+
+console.log(firstName);
+
+console.log(typeof firstName);
+
+//number
+
+const num = 5;
+const float = 5.5;
+console.log(typeof num);
+console.log(typeof float);
+
+const infinity = 4/0;
+const notANumber = 'asd' * 8;
+
+console.log(infinity);
+console.log(notANumber);
+console.log(typeof infinity);
+console.log(typeof notANumber);
+
+//Boolean
+
+const isMarried = true;
+const hasDog = false;
+
+console.log(typeof isMarried);
+console.log(typeof hasDog);
+
+//BigInt & Symbol
+
+const symb = Symbol();
+const bigint = BigInt(2);
+console.log(typeof symb);
+console.log(typeof bigint);
+
+//null && undefined
+
+let notValue;
+console.log(notValue);
+console.log(typeof notValue);
+
+const nothing = null;
+console.log(nothing);
+console.log(typeof nothing);
+
+//object
+
+const obj = {};
+console.log(typeof obj);
+
+const arr = [];
+const fn = function () {}
+console.log(typeof arr);
+console.log(typeof fn);*/
+
+/*
+console.log('Maroon ' + 5);
+console.log('5' + '5');
+console.log(+'5' + 5);
+
+
+console.log(Boolean(''));
+console.log(Boolean(' '));
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(!!'asd');
+console.log(!'asd');*/
+
+/*
+console.log(10 % 7);
+
+let age = 27;
+console.log(++age);
+
+c = c + a;
+c += a;*/
