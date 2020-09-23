352a4668e100e510fa69ec9e2ceff05ab7ea5776
Amateo642 <KitlyaevDD@mail.ru>
2020-09-23 17:07:25 +0300

    homework

--- main.js
+++ main.js
@@ -1,81 +1,24 @@
-/*
-//string
-let firstName = 'Zar';
-firstName = 'Amateo';
-const lastName = 'Goodman';
+const firstRow = 'mom washed the window';
+const secondRow = 'dog man friend';
 
-console.log(firstName);
+function getRow(firstRow, secondRow) {
+    let firstRowCounter = 0;
+    let secondRowCounter = 0;
+    for (let i = 0; i < firstRow.length; i++) {
+        if (firstRow.charAt(i) === 'o') {
+            firstRowCounter++;
+        }
+    }
+    for (let i = 0; i < secondRow.length; i++) {
+        if (secondRow.charAt(i) === 'o') {
+            secondRowCounter++;
+        }
+    }
+    if (firstRowCounter > secondRowCounter) {
+        return firstRow;
+    } else {
+        return secondRow;
+    }
+}
 
-console.log(typeof firstName);
-
-//number
-
-const num = 5;
-const float = 5.5;
-console.log(typeof num);
-console.log(typeof float);
-
-const infinity = 4/0;
-const notANumber = 'asd' * 8;
-
-console.log(infinity);
-console.log(notANumber);
-console.log(typeof infinity);
-console.log(typeof notANumber);
-
-//Boolean
-
-const isMarried = true;
-const hasDog = false;
-
-console.log(typeof isMarried);
-console.log(typeof hasDog);
-
-//BigInt & Symbol
-
-const symb = Symbol();
-const bigint = BigInt(2);
-console.log(typeof symb);
-console.log(typeof bigint);
-
-//null && undefined
-
-let notValue;
-console.log(notValue);
-console.log(typeof notValue);
-
-const nothing = null;
-console.log(nothing);
-console.log(typeof nothing);
-
-//object
-
-const obj = {};
-console.log(typeof obj);
-
-const arr = [];
-const fn = function () {}
-console.log(typeof arr);
-console.log(typeof fn);*/
-
-/*
-console.log('Maroon ' + 5);
-console.log('5' + '5');
-console.log(+'5' + 5);
-
-
-console.log(Boolean(''));
-console.log(Boolean(' '));
-console.log(Boolean(0));
-console.log(Boolean(null));
-console.log(!!'asd');
-console.log(!'asd');*/
-
-/*
-console.log(10 % 7);
-
-let age = 27;
-console.log(++age);
-
-c = c + a;
-c += a;*/
+console.log(getRow(firstRow, secondRow));
