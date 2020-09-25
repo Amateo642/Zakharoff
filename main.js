b2f2d136963e9cdf3bf736f683c7de9a24fb04f0
Amateo642 <KitlyaevDD@mail.ru>
2020-09-26 02:50:36 +0300

    HomeWork3

--- main.js
+++ main.js
@@ -1,24 +1,78 @@
-const firstRow = 'mom washed the window';
-const secondRow = 'dog man friend';
+const $btn = document.getElementById('btn-kick');
 
-function getRow(firstRow, secondRow) {
-    let firstRowCounter = 0;
-    let secondRowCounter = 0;
-    for (let i = 0; i < firstRow.length; i++) {
-        if (firstRow.charAt(i) === 'o') {
-            firstRowCounter++;
-        }
-    }
-    for (let i = 0; i < secondRow.length; i++) {
-        if (secondRow.charAt(i) === 'o') {
-            secondRowCounter++;
-        }
-    }
-    if (firstRowCounter > secondRowCounter) {
-        return firstRow;
-    } else {
-        return secondRow;
-    }
+const createPokemon = (name, defaultHP, damageHP, elHP, elProgressbar) => ({
+    name,
+    defaultHP,
+    damageHP,
+    elHP,
+    elProgressbar,
+}); 
+
+const character = createPokemon(
+    'Pikachu',
+    100,
+    100,
+    document.getElementById('health-character'),
+    document.getElementById('progressbar-character')
+);
+
+const enemy = createPokemon(
+    'Charmander', 
+    100, 
+    100, 
+    document.getElementById('health-enemy'), 
+    document.getElementById('progressbar-enemy')
+);   
+
+$btn.addEventListener('click', function () {
+    console.log('Kick');
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+});
+
+const $ult = document.createElement('button');
+$ult.classList.add('button');
+$ult.style.marginTop = '20px';
+$ult.innerHTML = 'Ultimate';
+$ult.addEventListener('click', function () {
+    changeHP(100, enemy);
+});
+$btn.parentElement.style.flexDirection = 'column';
+$btn.parentElement.appendChild($ult);
+
+function init() {
+    console.log('Start Game!');
+    renderHP(character);
+    renderHP(enemy);
 }
 
-console.log(getRow(firstRow, secondRow));
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function changeHP(count, person) {
+    if (person.damageHP < count) {
+        person.damageHP = 0;
+        alert('Poor fellow ' + person.name + ', lose fight!');
+        $btn.disabled = true;
+        $ult.disabled = true;
+    } else {
+        person.damageHP -= count;
+    }
+    renderHP(person);
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
+
+init();
